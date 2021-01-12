d4af05bf3090b10363338b98139a12fff26b4f73
chenery <16675365275@163.com>
2021-01-12 20:01:42 +0800

    读取和写入文件

---
+++ day02-NodeJs/03.读写练习/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+
+let str = '';
+fs.readFile('./doc.txt', (err, data) => {
+  if (err) console.log(err)
+  else str = data.toString(); fn()
+})
+
+async function fn() {
+  for (let v of str) {
+    await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        fs.writeFile('./doc1.txt', v, {
+          flag:'a'
+        }, (err) => {
+            if (err) reject(err)
+            else resolve()
+        })
+      }, 200)
+    }).catch((err) => {
+      console.log(err)
+    })
+  }
+}
