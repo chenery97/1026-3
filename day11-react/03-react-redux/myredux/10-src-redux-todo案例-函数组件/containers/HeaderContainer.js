92d4cb93fa787d704424618bf590633ef26832cf
chenery <16675365275@163.com>
2021-02-02 17:52:43 +0800

    react-redux-todo

--- day11-react/03-react-redux/myredux/10-src-redux-todo案例-函数组件/containers/HeaderContainer.js
+++ day11-react/03-react-redux/myredux/10-src-redux-todo案例-函数组件/containers/HeaderContainer.js
@@ -2,4 +2,4 @@ import { connect } from 'react-redux'
 import Header from '../pages/Header/Header'
 import { addTodo } from '../redux/actions'
 
-export default connect(state => state, { addTodo })(Header)
+export default connect(null, { addTodo })(Header)
